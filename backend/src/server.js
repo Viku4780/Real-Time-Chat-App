6e27101ae769d8e40ae52b26ba2779d0baa77d02
Vikrant <vikrantmom5748@gmail.com>
2026-04-14 12:25:12 +0530

    signup implemented in frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import path from 'path';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js'
+import { ENV } from './lib/env.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes)
