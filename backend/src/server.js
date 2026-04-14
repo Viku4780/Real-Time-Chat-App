b3ce491b306f608faf5092107ffb21c8dc504060
Vikrant <vikrantmom5748@gmail.com>
2026-04-14 08:48:03 +0530

    message route added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js';
 import path from 'path';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import messageRoutes from './routes/message.route.js'
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes)
 
 // make ready for deployment
 if(process.env.NODE_ENV === "production"){
