ce53245c7f2e638e841d576290bf9946a5ee9230
Vikrant <vikrantmom5748@gmail.com>
2026-04-16 10:02:33 +0530

    sidebar implemented

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
-export const useChatStore = create(() => ({
+export const useChatStore = create((set, get) => ({
     allContacts: [],
     chats: [],
     messages: [],
@@ -10,11 +10,11 @@ export const useChatStore = create(() => ({
     selectedUser: null,
     isUsersLoading: false,
     isMessagesLoading: false,
-    isSoundEnabled: localStorage.getItem("isSoundEnabled") === true,
+    isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
 
     toggleSound: () => {
-        localStorage.setItem("isSooundEnabled", !get().isSoundEnabled);
-        setInterval({isSoundEnabled: !get().isSoundEnabled})
+        localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
+        set({isSoundEnabled: !get().isSoundEnabled})
     },
 
     setActiveTab: (tab) => set({ activeTab: tab}),
