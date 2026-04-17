d55d6015dc0771608164e7ae1e0b31b6fac0008c
Vikrant <vikrantmom5748@gmail.com>
2026-04-17 09:49:16 +0530

    implement realtime socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,11 @@ import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js'
 import { ENV } from './lib/env.js';
 import cors from 'cors';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
-const app = express();
+// const app = express();
 const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3000;
@@ -31,7 +32,7 @@ if(process.env.NODE_ENV === "production"){
     })
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
     connectDB();
 })
--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -2,31 +2,35 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import UsersLoadingSkeleton from './UsersLoadingSkeleton';
 import NoChatsFound from './NoChatsFound';
+import { useAuthStore } from '../store/useAuthStore';
 
 const ChatsList = () => {
-  const {getMyChatPartners, chats, isUsersLoading, setSelectedUser} = useChatStore();
+  const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getMyChatPartners();
-  },[getMyChatPartners]);
+  }, [getMyChatPartners]);
 
-  if(isUsersLoading) return <UsersLoadingSkeleton />;
-  if(chats.length === 0) return <NoChatsFound />;
+  if (isUsersLoading) return <UsersLoadingSkeleton />;
+  if (chats.length === 0) return <NoChatsFound />;
 
   return (
-   <>
-    {chats.map(chat => (
-      <div key={chat._id} className='bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors' onClick={() => setSelectedUser(chat)}>
-        {/* TODO: FIX THIS ONLINE STATUS AND MAKE IT WORK WITH SOCKET */}
-        <div className={`avatar-online`}>
-          <div className='size-12 rounded-full'>
-            <img src={chat.profilePic || "/avatar.png"} alt={chat.fullName} />
+    <>
+      {chats.map(chat => (
+        <div key={chat._id} className='bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors' onClick={() => setSelectedUser(chat)}>
+          {/* TODO: FIX THIS ONLINE STATUS AND MAKE IT WORK WITH SOCKET */}
+          <div className='flex items-center gap-3'>
+            <div className={`avatar ${onlineUsers.includes(chat._id) ? "online" : 'offline'}`}>
+              <div className='size-12 rounded-full'>
+                <img src={chat.profilePic || "/avatar.png"} alt={chat.fullName} />
+              </div>
+            </div>
+            <h4 className='text-slate-200 font-medium truncate'>{chat.fullName}</h4>
           </div>
         </div>
-        <h4 className='text-slate-200 font-medium truncate'>{chat.fullName}</h4>
-      </div>
-    ))}
-   </>
+      ))}
+    </>
   )
 }
 
