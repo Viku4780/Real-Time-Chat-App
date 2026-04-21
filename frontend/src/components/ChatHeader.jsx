50e670d52b7cf8e8419499b83bccf7bec5cfddf1
Vikrant <vikrantmom5748@gmail.com>
2026-04-21 13:01:49 +0530

    improve chat header and sendMesssage controller

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -4,34 +4,38 @@ import { XIcon } from 'lucide-react';
 import { useAuthStore } from '../store/useAuthStore';
 
 const ChatHeader = () => {
-    const {selectedUser, setSelectedUser} = useChatStore();
-    const {onlineUsers} = useAuthStore();
-    const isOnline = onlineUsers.includes(selectedUser._id);
+  const { selectedUser, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
+  const isOnline = onlineUsers.includes(selectedUser._id);
 
-    useEffect(() => {
-        const handleEscKey = (e) => {
-            if(e.key === "Escape") setSelectedUser(null);
-        }
+  useEffect(() => {
+    const handleEscKey = (e) => {
+      if (e.key === "Escape") setSelectedUser(null);
+    }
 
-        window.addEventListener("keydown", handleEscKey);
+    window.addEventListener("keydown", handleEscKey);
 
-        // cleanup function
-        return () => window.removeEventListener("keydown", handleEscKey);
-    },[setSelectedUser]);
+    // cleanup function
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [setSelectedUser]);
 
   return (
     <div className='flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1'>
-      <div className='flex items-center space-x-3'>
-        <div className={`avatar ${isOnline ? "online" : "offline"}`}>
-            <div>
-                <img src={selectedUser.profilePic || "/avatar.png"} alt={selectedUser.fullName} />
-            </div>
+      <div className='flex  items-center space-x-3'>
+
+        <div className={`avatar ${isOnline ? "online" : "offline"} h-[60px]`}>
+          <img src={selectedUser.profilePic || "/avatar.png"} alt={selectedUser.fullName} className='w-[20px]' />
+          {/* <div> */}
+          {/* </div> */}
         </div>
+
         <div>
-            <h3 className='text-slate-200 font-medium'>{selectedUser.fullName}</h3>
-            <p className='text-slate-400 text-sm'>{`avatar ${isOnline ? "Online" : "Offline"}`}</p>
+          <h3 className='text-slate-200 font-medium'>{selectedUser.fullName}</h3>
+          <p className='text-slate-400 text-xs'>{`${isOnline ? "Online" : "Offline"}`}</p>
         </div>
+
       </div>
+
       <button onClick={() => setSelectedUser(null)}>
         <XIcon className='w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer' />
       </button>
