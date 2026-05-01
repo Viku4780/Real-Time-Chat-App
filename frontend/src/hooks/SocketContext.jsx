73b7536b297d0f43748dd66aed63d91f8f4648ff
Vikrant <vikrantmom5748@gmail.com>
2026-05-01 12:51:02 +0530

    refactor socketcontext

--- frontend/src/hooks/SocketContext.jsx
+++ frontend/src/hooks/SocketContext.jsx
@@ -5,55 +5,56 @@ import { subscribeToMessages } from '../store/slices/chatSlice';
 
 const SocketContext = createContext(null);
 
-const protocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
-const host = window.location.host; // Includes domain and port (e.g., localhost:3000)
-const socketUrl = `${protocol}${host}/ws`; // Adjust path (/ws) to match your backend
-
-
 
 export const SocketProvider = ({ children }) => {
   const socketRef = useRef(null);
   const dispatch = useDispatch();
 
-  const {user} = useSelector(state => state.auth);
+  const protocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+  const host = window.location.host; // Includes domain and port (e.g., localhost:3000)
+  const socketUrl = `${protocol}${host}`; // Adjust path (/ws) to match your backend
 
-   useEffect(() => {
-      // only connect if user exists and there's no active connection
-      // readystate 0 = connecting, 1 = open
-      if (user && (!socketRef.current || socketRef.current?.readyState >= 2)) {
-        const socket = new WebSocket(socketUrl);
-  
-        socket.onopen = () => {
-          console.log("Connected");
-          socketRef.current = socket;
-        }
+  console.log(socketUrl);
 
-        socket.onmessage = (e) => {
-          const data = JSON.parse(e.data);
-       
-          if(data.type === "getOnlineUsers"){
-            dispatch(updateOnlineUser(data.payload));
-          }
+  const { user } = useSelector(state => state.auth);
 
-          if(data.type === "newMessage"){
-            
-            dispatch(subscribeToMessages(data.payload));
-          }
-        }
-  
-        socket.onclose = (e) => {
-          console.log("Disconnected:", e.reason);
-          socketRef.current = null; // reset ref so it can reconnect if needed 
-          // setSocketObj(null)
-        };
-  
-        return () => {
-          if (socket.readyState === 1) {
-            socket.close();
-          }
-        };
+  useEffect(() => {
+    // only connect if user exists and there's no active connection
+    // readystate 0 = connecting, 1 = open
+    if (user && (!socketRef.current || socketRef.current?.readyState >= 2)) {
+      const socket = new WebSocket(window.location.protocol === 'https:' ? socketUrl : 'ws://localhost:3000');
+
+      socket.onopen = () => {
+        console.log("Connected");
+        socketRef.current = socket;
       }
-    }, [user]);
+
+      socket.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+
+        if (data.type === "getOnlineUsers") {
+          dispatch(updateOnlineUser(data.payload));
+        }
+
+        if (data.type === "newMessage") {
+
+          dispatch(subscribeToMessages(data.payload));
+        }
+      }
+
+      socket.onclose = (e) => {
+        console.log("Disconnected:", e.reason);
+        socketRef.current = null; // reset ref so it can reconnect if needed 
+        // setSocketObj(null)
+      };
+
+      return () => {
+        if (socket.readyState === 1) {
+          socket.close();
+        }
+      };
+    }
+  }, [user]);
 
   return (
     <SocketContext.Provider value={socketRef.current}>
