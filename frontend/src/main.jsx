50e864f7bb27bec7f60085b430793b46b514a176
Vikrant <vikrantmom5748@gmail.com>
2026-06-08 13:52:12 +0530

    remove socket provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,12 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux';
 import store from './store/store.js'
-import { SocketProvider } from './hooks/SocketContext'
 
 createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
       <Provider store={store}>
-        <SocketProvider>
           <App />
-        </SocketProvider>
       </Provider>
     </BrowserRouter>
  ,
