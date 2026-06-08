0dbf08003ad64110b6a8c5782fbe2f7e30113020
Vikrant <vikrantmom5748@gmail.com>
2026-06-08 13:51:55 +0530

    refactor store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import chatReducer from '../features/chat/chatSlice'
+import chatReducer from '../features/conversations/chatSlice'
 
 const store = configureStore({
     reducer: {
