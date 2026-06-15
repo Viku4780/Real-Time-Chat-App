70427ab72f269314c69294b5f7d90da7d02e4a83
Vikrant <vikrantmom5748@gmail.com>
2026-06-15 08:55:10 +0530

    implemented websocket auth handler

---
+++ backend/src/features/realtime/middleware/websocketAuth.js
@@ -0,0 +1,27 @@
+import { ENV } from "../../../config/env.js";
+import User from "../../auth/auth.model.js";
+import jwt from 'jsonwebtoken';
+
+export const socketAuth = (handler) => {
+    return async (socket, req) => {
+        try {
+            const token = req.headers.cookie?.split("; ").find((row) => row.startsWith("jwt="))?.split("=")[1];
+            if (!token) throw new Error('No token provided');
+
+            const decoded = jwt.verify(token, ENV.JWT_SECRET);
+            if (!decoded) throw new Error("Unauthorized - Invalid token");
+
+            const user = await User.findById(decoded.userId);
+            if (!user) throw new Error("User not found")
+
+            socket.user = user;
+            socket.userId = user._id.toString();
+
+            // run the main logic only if auth passes
+            await handler(socket, req);
+        } catch (error) {
+            console.error("Socket Auth Error:", error.message);
+            socket.terminate();
+        }
+    }
+}
