e1f2c4b10ce84f4a9183bb7254bb534f0f7bcc9d
Vikrant <vikrantmom5748@gmail.com>
2026-06-15 09:08:48 +0530

    added conversation and user reducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import chatReducer from '../features/conversations/chatSlice'
+import chatReducer from '../features/conversations/chatSlice';
+import conversationReducer from '../features/conversations/conversationSlice';
+import userReducer from '../features/users/userSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        chat: chatReducer
+        chat: chatReducer,
+        conversation : conversationReducer,
+        user: userReducer,
     }
 });
 
