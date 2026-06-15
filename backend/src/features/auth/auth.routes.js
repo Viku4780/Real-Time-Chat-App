a93615b5c422d0ea214f27bc0566118820fa760f
Vikrant <vikrantmom5748@gmail.com>
2026-06-15 08:50:22 +0530

    remove rate-limiting from login route

--- backend/src/features/auth/auth.routes.js
+++ backend/src/features/auth/auth.routes.js
@@ -8,11 +8,12 @@ const router = express.Router();
 router.use(arcjetProtection);
 
 router.post("/signup", signup);
-router.post("/login",arcjetProtection, login);
+router.post("/login", login);
 router.post('/logout', logout);
 
 router.put("/update-profile",protectRoute, updateProfile);
 
 router.get("/check", protectRoute, (req,res) => res.status(200).json(req.user));
+
 
 export default router;
