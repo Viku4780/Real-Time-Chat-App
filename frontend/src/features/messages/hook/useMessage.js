0c0e32191d5b63f02381635fced446f223b5a532
Vikrant <vikrantmom5748@gmail.com>
2026-06-15 09:04:36 +0530

    refactor message hook and implemented addNewMessage and updateChatList redux request

--- frontend/src/features/messages/hook/useMessage.js
+++ frontend/src/features/messages/hook/useMessage.js
@@ -1,8 +1,10 @@
 import React, { useRef, useState } from 'react'
 import useKeyboardSound from './useKeyboardSound'
 import { useDispatch, useSelector } from 'react-redux';
-import { sendMessage } from '../../conversations/chatSlice';
+import { sendMessage,addingNewMessage } from '../../conversations/chatSlice';
 import toast from 'react-hot-toast';
+import { dateGenerator } from '../../../utils/dateGenerator';
+import { updateChatList } from '../../conversations/conversationSlice';
 
 const useMessage = () => {
     const { playRandomKeyStrokeSound } = useKeyboardSound();
@@ -12,16 +14,41 @@ const useMessage = () => {
     const fileInputRef = useRef(null);
 
     const { isSoundEnabled } = useSelector(state => state.chat);
+    const {activeConversation} = useSelector(state => state.conversation);
+    const {selectedUser} = useSelector(state => state.user);
+    const {user} = useSelector(state => state.auth);
     const dispatch = useDispatch();
-
+    
     const handleSendMessage = (e) => {
         e.preventDefault();
         if (!text.trim() && !imagePreview) return;
         if (isSoundEnabled) playRandomKeyStrokeSound();
+        
+        const uniqueId = crypto.randomUUID();
+        dispatch(addingNewMessage({
+            _id: uniqueId,
+            senderId: user._id,
+            conversationId: activeConversation,
+            text: text.trim(),
+            image: imagePreview,
+            status: "pending",
+            createdAt: dateGenerator()
+        }))
+
+        dispatch(updateChatList({
+            _id: uniqueId,
+            senderId: user._id,
+            conversationId: activeConversation,
+            text: text.trim(),
+            createdAt: dateGenerator()
+        }))
 
         dispatch(sendMessage({
             text: text.trim(),
+            temporaryId: uniqueId,
             image: imagePreview,
+            conversationId: activeConversation,
+            receiverId: selectedUser._id
         }));
         setText("");
         setImagePreview("");
