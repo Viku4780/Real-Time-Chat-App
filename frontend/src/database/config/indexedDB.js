383b7dc1baf857d8820e59424eb066af3f2463e5
Vikrant <vikrantmom5748@gmail.com>
2026-06-22 12:34:57 +0530

    created indexedDB database instance

--- frontend/src/database/config/indexedDB.js
+++ frontend/src/database/config/indexedDB.js
@@ -1,1 +1,9 @@
-import { IDBStore } from "../service/indexedDB.service";
+import {Dexie} from 'dexie';
+
+const db = new Dexie("chatify");
+
+db.version(1).stores({
+    messages: "++_id, conversationId, senderId, isActive, createdAt"
+})
+
+export {db};
